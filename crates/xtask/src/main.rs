e621529ad030bdb0be49dd707f047968805c7e00
bouzuya <m@bouzuya.net>
2024-07-25 20:35:20 +0900

    Add build subcommand

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(clap::Parser)]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(clap::Subcommand)]
+enum Subcommand {
+    Build,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = <Cli as clap::Parser>::parse();
+    match cli.subcommand {
+        Subcommand::Build => {
+            println!("Build");
+        }
+    }
 }
