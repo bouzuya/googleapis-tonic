11fa8d1ac3ef3d913deaed28e0849445ac2679cd
bouzuya <m@bouzuya.net>
2024-08-08 20:44:27 +0900

    Add ProtoDir::emit_package_names

--- crates/xtask/src/proto_dir.rs
+++ crates/xtask/src/proto_dir.rs
@@ -12,6 +12,7 @@ use crate::{proto_file::ProtoFile, protobuf_package_name::ProtobufPackageName};
 pub struct ProtoDir {
     dependencies: BTreeMap<ProtobufPackageName, BTreeSet<ProtobufPackageName>>,
     dir_path: PathBuf,
+    emit_package_names: BTreeSet<ProtobufPackageName>,
     proto_paths: Vec<PathBuf>,
 }
 
@@ -20,11 +21,31 @@ impl ProtoDir {
         let dir_path: PathBuf = proto_dir.into();
 
         let mut all_proto_files = BTreeMap::<PathBuf, ProtoFile>::new();
-        let proto_paths = Self::proto_paths_from_dir(&dir_path)?;
-        for path in &proto_paths {
+        let proto_file_paths = Self::proto_file_paths_from_dir(&dir_path)?;
+        for path in &proto_file_paths {
             let content = fs::read_to_string(path)?;
             let proto_file = ProtoFile::from_str(&content)?;
             all_proto_files.insert(path.strip_prefix(&dir_path)?.to_owned(), proto_file);
+        }
+
+        let mut emit_package_name_candidates = BTreeSet::<ProtobufPackageName>::new();
+        for proto_dir_path in Self::proto_dir_paths_from_dir(&dir_path)? {
+            let package_name = proto_dir_path
+                .strip_prefix(&dir_path)?
+                .to_owned()
+                .to_str()
+                .context("proto_dir_path is not valid UTF-8")?
+                .split(std::path::MAIN_SEPARATOR)
+                .collect::<Vec<&str>>()
+                .join(".");
+            match ProtobufPackageName::from_str(&package_name) {
+                Err(_) => {
+                    // skip invalid package name
+                }
+                Ok(package_name) => {
+                    emit_package_name_candidates.insert(package_name);
+                }
+            }
         }
 
         let mut dependencies =
@@ -38,16 +59,27 @@ impl ProtoDir {
         for (package_name, deps) in dependencies.iter_mut() {
             deps.remove(package_name);
         }
+        let mut emit_package_names = BTreeSet::<ProtobufPackageName>::new();
+        for package_name in dependencies.keys() {
+            if emit_package_name_candidates.contains(&package_name) {
+                emit_package_names.insert(package_name.to_owned());
+            }
+        }
 
         Ok(Self {
             dependencies,
             dir_path,
-            proto_paths,
+            emit_package_names,
+            proto_paths: proto_file_paths,
         })
     }
 
     pub fn dependencies(&self) -> &BTreeMap<ProtobufPackageName, BTreeSet<ProtobufPackageName>> {
         &self.dependencies
+    }
+
+    pub fn emit_package_names(&self) -> &BTreeSet<ProtobufPackageName> {
+        &self.emit_package_names
     }
 
     pub fn dir_path(&self) -> &Path {
@@ -87,7 +119,19 @@ impl ProtoDir {
             )
     }
 
-    fn proto_paths_from_dir<P: AsRef<Path>>(dir: P) -> anyhow::Result<Vec<PathBuf>> {
+    fn proto_dir_paths_from_dir<P: AsRef<Path>>(dir: P) -> anyhow::Result<Vec<PathBuf>> {
+        let dir: &Path = dir.as_ref();
+        let mut paths = vec![];
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            if entry.file_type()?.is_dir() {
+                paths.push(entry.path());
+                paths.append(&mut Self::proto_dir_paths_from_dir(entry.path())?);
+            }
+        }
+        Ok(paths)
+    }
+    fn proto_file_paths_from_dir<P: AsRef<Path>>(dir: P) -> anyhow::Result<Vec<PathBuf>> {
         let dir: &Path = dir.as_ref();
         let mut paths = vec![];
         for entry in fs::read_dir(dir)? {
@@ -101,7 +145,7 @@ impl ProtoDir {
                 }
             } else {
                 let path_buf = entry.path();
-                paths.append(&mut Self::proto_paths_from_dir(&path_buf)?);
+                paths.append(&mut Self::proto_file_paths_from_dir(&path_buf)?);
             }
         }
         Ok(paths)
