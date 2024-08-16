4dfe6d908f52f77df946f44a03c1a45b3d3c580e
bouzuya <m@bouzuya.net>
2024-08-17 08:30:37 +0900

    Fix to remove generated/ before building

--- crates/xtask/src/command/build.rs
+++ crates/xtask/src/command/build.rs
@@ -2,6 +2,7 @@ mod build_crate;
 mod build_crates;
 mod state;
 
+use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr as _;
 
@@ -42,12 +43,19 @@ use crate::proto_dir::ProtoDir;
 pub fn execute() -> anyhow::Result<()> {
     let generated_dir = PathBuf::from("generated");
     let xtask_dir = PathBuf::from("crates").join("xtask");
+
+    // load googleapis/
     let proto_dir = xtask_dir.join("googleapis");
     let proto_dir = ProtoDir::load(proto_dir)?;
 
+    // load state.json
     let state_file = xtask_dir.join("state.json");
     let state = State::load(&state_file)?;
 
+    // remove generated/
+    fs::remove_dir_all(&generated_dir)?;
+
+    // generate generated/
     let crate_versions = state.crate_versions();
     let crate_version = crate_versions
         .get(&CrateName::from_str("googleapis-tonic")?)
@@ -58,6 +66,7 @@ pub fn execute() -> anyhow::Result<()> {
         build_crates::build_crates(&generated_dir, &proto_dir, crate_versions)?;
     new_crate_versions.insert(CrateName::from_str("googleapis-tonic")?, new_crate_version);
 
+    // update state.json
     let updated = state.update(&proto_dir, new_crate_versions)?;
     State::save(&state_file, &updated)?;
     Ok(())
