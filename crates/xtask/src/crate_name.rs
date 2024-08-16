6318d5f5d9dd5f4f10655c481aaabd4788836c91
bouzuya <m@bouzuya.net>
2024-08-16 12:45:26 +0900

    Add impl FromStr for CrateName

--- crates/xtask/src/crate_name.rs
+++ crates/xtask/src/crate_name.rs
@@ -3,7 +3,7 @@ use crate::protobuf_package_name::ProtobufPackageName;
 /// A crate name.
 ///
 /// e.g. `googleapis-tonic-foo-bar-baz`
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct CrateName(String);
 
 impl CrateName {
@@ -28,5 +28,14 @@ impl AsRef<str> for CrateName {
 impl std::fmt::Display for CrateName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
+    }
+}
+
+impl std::str::FromStr for CrateName {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // TODO: check format
+        Ok(Self(s.to_owned()))
     }
 }
