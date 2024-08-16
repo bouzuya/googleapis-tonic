aa7b664a6e356b3e64edff5d63dccb7c3b453268
bouzuya <m@bouzuya.net>
2024-08-16 12:45:59 +0900

    Fix to use crate_version

--- crates/xtask/src/command/build/state.rs
+++ crates/xtask/src/command/build/state.rs
@@ -2,10 +2,12 @@ use std::{
     collections::{BTreeMap, HashMap, VecDeque},
     fs,
     path::Path,
+    str::FromStr,
 };
 
 use crate::{
-    crate_name::CrateName, proto_dir::ProtoDir, protobuf_package_name::ProtobufPackageName,
+    crate_name::CrateName, crate_version::CrateVersion, googleapis_version::GoogleapisVersion,
+    proto_dir::ProtoDir, protobuf_package_name::ProtobufPackageName,
 };
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -16,51 +18,104 @@ struct StateFileContent {
     publish_order: Vec<String>,
 }
 
-pub struct State(StateFileContent);
+impl From<&State> for StateFileContent {
+    fn from(
+        State {
+            crate_versions,
+            googleapis_version,
+            next_version,
+            publish_order,
+        }: &State,
+    ) -> Self {
+        Self {
+            crate_versions: crate_versions
+                .into_iter()
+                .map(|(k, v)| (k.to_string(), v.to_string()))
+                .collect::<BTreeMap<String, String>>(),
+            googleapis_version: googleapis_version.to_string(),
+            next_version: next_version.to_string(),
+            publish_order: publish_order
+                .into_iter()
+                .map(|it| it.to_string())
+                .collect::<Vec<String>>(),
+        }
+    }
+}
+
+impl TryFrom<&StateFileContent> for State {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        StateFileContent {
+            crate_versions,
+            googleapis_version,
+            next_version,
+            publish_order,
+        }: &StateFileContent,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            crate_versions: crate_versions
+                .into_iter()
+                .map(|(k, v)| Ok((CrateName::from_str(k)?, CrateVersion::from_str(v)?)))
+                .collect::<anyhow::Result<BTreeMap<CrateName, CrateVersion>>>()?,
+            googleapis_version: GoogleapisVersion::from_str(&googleapis_version)?,
+            next_version: CrateVersion::from_str(&next_version)?,
+            publish_order: publish_order
+                .into_iter()
+                .map(|it| CrateName::from_str(it))
+                .collect::<anyhow::Result<Vec<CrateName>>>()?,
+        })
+    }
+}
+
+pub struct State {
+    crate_versions: BTreeMap<CrateName, CrateVersion>,
+    googleapis_version: GoogleapisVersion,
+    next_version: CrateVersion,
+    publish_order: Vec<CrateName>,
+}
 
 impl State {
     // TODO: remove I/O
     pub fn load(state_file: &Path) -> anyhow::Result<Self> {
         let content = fs::read_to_string(&state_file)?;
-        Ok(Self(serde_json::from_str::<StateFileContent>(&content)?))
+        let content = serde_json::from_str::<StateFileContent>(&content)?;
+        State::try_from(&content)
     }
 
     // TODO: remove I/O
     pub fn save(state_file: &Path, state: &State) -> anyhow::Result<()> {
-        let content = serde_json::to_string_pretty(&state.0)?;
+        let content = StateFileContent::from(state);
+        let content = serde_json::to_string_pretty(&content)?;
         fs::write(state_file, &content)?;
         Ok(())
     }
 
+    // TODO: use CrateVersion
     pub fn current_version(&self) -> String {
-        self.0.next_version.clone()
+        self.next_version.to_string()
     }
 
     pub fn update(&self, proto_dir: &ProtoDir) -> anyhow::Result<Self> {
-        let next_version = &self.0.next_version;
-        let state = StateFileContent {
-            crate_versions: self
-                .0
-                .crate_versions
-                .keys()
-                .into_iter()
-                .map(|key| (key.to_owned(), next_version.to_string()))
-                .collect::<BTreeMap<String, String>>(),
-            googleapis_version: proto_dir.version().to_string(),
-            next_version: Self::next_version(&self.0.next_version)?,
-            publish_order: Self::publish_order(proto_dir),
-        };
-        Ok(Self(state))
+        let next_version = &self.next_version;
+        let crate_versions = self
+            .crate_versions
+            .keys()
+            .into_iter()
+            .map(|key| (key.to_owned(), next_version.to_owned()))
+            .collect::<BTreeMap<CrateName, CrateVersion>>();
+        let googleapis_version = proto_dir.version().to_owned();
+        let next_version = self.next_version.increment_minor();
+        let publish_order = Self::publish_order(proto_dir);
+        Ok(Self {
+            crate_versions,
+            googleapis_version,
+            next_version,
+            publish_order,
+        })
     }
 
-    fn next_version(version: &str) -> anyhow::Result<String> {
-        let version = semver::Version::parse(version)?;
-        let next_version =
-            semver::Version::new(version.major, version.minor + 1, version.patch).to_string();
-        Ok(next_version)
-    }
-
-    fn publish_order(proto_dir: &ProtoDir) -> Vec<String> {
+    fn publish_order(proto_dir: &ProtoDir) -> Vec<CrateName> {
         // topological sort
         let nodes = proto_dir.emit_package_names().to_owned();
         let mut edges = BTreeMap::new();
@@ -100,7 +155,6 @@ impl State {
         sorted
             .iter()
             .map(CrateName::from_package_name)
-            .map(|it| it.to_string())
-            .collect::<Vec<String>>()
+            .collect::<Vec<CrateName>>()
     }
 }
