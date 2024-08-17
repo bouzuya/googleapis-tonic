61ad50357ab6ea2d20b32b92728e8de40a19091c
bouzuya <m@bouzuya.net>
2024-08-17 23:06:22 +0900

    Add ProtoDir::package_hashes

--- crates/xtask/src/proto_dir.rs
+++ crates/xtask/src/proto_dir.rs
@@ -1,4 +1,3 @@
-use crate::{googleapis_version::GoogleapisVersion, proto_file_path::ProtoFilePath};
 use std::{
     collections::{BTreeMap, BTreeSet, VecDeque},
     fs,
@@ -8,12 +7,17 @@ use std::{
 
 use anyhow::Context as _;
 
-use crate::{proto_file::ProtoFile, protobuf_package_name::ProtobufPackageName};
+use crate::googleapis_version::GoogleapisVersion;
+use crate::proto_file::ProtoFile;
+use crate::proto_file_path::ProtoFilePath;
+use crate::protobuf_package_name::ProtobufPackageName;
+use crate::sha1hash::Sha1Hash;
 
 pub struct ProtoDir {
-    dependencies: BTreeMap<ProtobufPackageName, BTreeSet<ProtobufPackageName>>,
     dir_path: PathBuf,
     emit_package_names: BTreeSet<ProtobufPackageName>,
+    package_deps: BTreeMap<ProtobufPackageName, BTreeSet<ProtobufPackageName>>,
+    package_hashes: BTreeMap<ProtobufPackageName, Sha1Hash>,
     proto_file_paths: Vec<ProtoFilePath>,
     version: GoogleapisVersion,
 }
@@ -25,80 +29,101 @@ impl ProtoDir {
 
         let version = GoogleapisVersion::load_from_googleapis_dir(&dir_path)?;
         let proto_file_paths = Self::proto_file_paths_from_dir(&dir_path, &dir_path)?;
-        let proto_dir_paths = Self::proto_dir_paths_from_dir(&dir_path)?;
 
-        let mut all_proto_files = BTreeMap::<ProtoFilePath, ProtoFile>::new();
-        for proto_file_path in &proto_file_paths {
-            let content = fs::read_to_string(dir_path.join(proto_file_path.to_path_buf()))?;
-            let proto_file = ProtoFile::from_str(&content)?;
-            all_proto_files.insert(proto_file_path.to_owned(), proto_file);
-        }
-
-        let mut emit_package_name_candidates = BTreeSet::<ProtobufPackageName>::new();
-        for proto_dir_path in proto_dir_paths {
-            let package_name = proto_dir_path
-                .strip_prefix(&dir_path)?
-                .to_owned()
-                .to_str()
-                .context("proto_dir_path is not valid UTF-8")?
-                .split(std::path::MAIN_SEPARATOR)
-                .collect::<Vec<&str>>()
-                .join(".");
-            match ProtobufPackageName::from_str(&package_name) {
-                Err(_) => {
-                    // skip invalid package name
+        let (package_deps, package_hashes) = {
+            let all_proto_files = {
+                let mut map = BTreeMap::<ProtoFilePath, ProtoFile>::new();
+                for proto_file_path in &proto_file_paths {
+                    let content = fs::read_to_string(dir_path.join(proto_file_path.to_path_buf()))?;
+                    let proto_file = ProtoFile::from_str(&content)?;
+                    map.insert(proto_file_path.to_owned(), proto_file);
                 }
-                Ok(package_name) => {
-                    emit_package_name_candidates.insert(package_name);
+                map
+            };
+
+            let mut package_deps =
+                BTreeMap::<ProtobufPackageName, BTreeSet<ProtobufPackageName>>::new();
+            for proto_file in all_proto_files.values() {
+                package_deps
+                    .entry(proto_file.package_name().to_owned())
+                    .or_default()
+                    .extend(Self::proto_file_dependencies(&all_proto_files, proto_file)?);
+            }
+            for (package_name, deps) in package_deps.iter_mut() {
+                deps.remove(package_name);
+            }
+
+            let package_hashes = BTreeMap::<ProtobufPackageName, Sha1Hash>::new();
+            // FIXME
+            (package_deps, package_hashes)
+        };
+
+        let emit_package_names = {
+            let emit_package_name_candidates = {
+                let mut set = BTreeSet::<ProtobufPackageName>::new();
+                let proto_dir_paths = Self::proto_dir_paths_from_dir(&dir_path)?;
+                for proto_dir_path in proto_dir_paths {
+                    let package_name = proto_dir_path
+                        .strip_prefix(&dir_path)?
+                        .to_owned()
+                        .to_str()
+                        .context("proto_dir_path is not valid UTF-8")?
+                        .split(std::path::MAIN_SEPARATOR)
+                        .collect::<Vec<&str>>()
+                        .join(".");
+                    match ProtobufPackageName::from_str(&package_name) {
+                        Err(_) => {
+                            // skip invalid package name
+                        }
+                        Ok(package_name) => {
+                            set.insert(package_name);
+                        }
+                    }
+                }
+                set
+            };
+
+            let mut emit_package_names = BTreeSet::<ProtobufPackageName>::new();
+            for package_name in package_deps.keys() {
+                if emit_package_name_candidates.contains(package_name) {
+                    emit_package_names.insert(package_name.to_owned());
                 }
             }
-        }
 
-        let mut dependencies =
-            BTreeMap::<ProtobufPackageName, BTreeSet<ProtobufPackageName>>::new();
-        for proto_file in all_proto_files.values() {
-            dependencies
-                .entry(proto_file.package_name().to_owned())
-                .or_default()
-                .extend(Self::proto_file_dependencies(&all_proto_files, proto_file)?);
-        }
-        for (package_name, deps) in dependencies.iter_mut() {
-            deps.remove(package_name);
-        }
-        let mut emit_package_names = BTreeSet::<ProtobufPackageName>::new();
-        for package_name in dependencies.keys() {
-            if emit_package_name_candidates.contains(package_name) {
-                emit_package_names.insert(package_name.to_owned());
-            }
-        }
-
-        // max package name length is 47. crates.io max crate name length is 64. googleapis-tonic- length is 17.
-        let mut invalid_package_names = dependencies
-            .keys()
-            .filter(|it| it.to_string().len() > 47)
-            .cloned()
-            .collect::<VecDeque<ProtobufPackageName>>();
-        while let Some(invalid_package_name) = invalid_package_names.pop_front() {
-            if emit_package_names.remove(&invalid_package_name) {
-                for (pkg, deps) in dependencies.iter() {
-                    if deps.contains(&invalid_package_name) {
-                        invalid_package_names.push_back(pkg.to_owned());
+            const CRATES_IO_CRATE_NAME_MAX_LENGTH: usize = 64;
+            const CRATE_NAME_PREFIX_LENGTH: usize = "googleapis-tonic-".len();
+            let mut invalid_package_names = package_deps
+                .keys()
+                .filter(|it| {
+                    CRATE_NAME_PREFIX_LENGTH + it.to_string().len()
+                        > CRATES_IO_CRATE_NAME_MAX_LENGTH
+                })
+                .cloned()
+                .collect::<VecDeque<ProtobufPackageName>>();
+            while let Some(invalid_package_name) = invalid_package_names.pop_front() {
+                if emit_package_names.remove(&invalid_package_name) {
+                    for (pkg, deps) in package_deps.iter() {
+                        if deps.contains(&invalid_package_name) {
+                            invalid_package_names.push_back(pkg.to_owned());
+                        }
                     }
                 }
             }
-        }
+            emit_package_names
+        };
 
         Ok(Self {
-            dependencies,
             dir_path,
             emit_package_names,
+            package_deps,
+            package_hashes,
             proto_file_paths,
             version,
         })
     }
 
     pub fn dependencies(&self) -> &BTreeMap<ProtobufPackageName, BTreeSet<ProtobufPackageName>> {
-        &self.dependencies
+        &self.package_deps
     }
 
     pub fn emit_package_names(&self) -> &BTreeSet<ProtobufPackageName> {
@@ -107,6 +132,10 @@ impl ProtoDir {
 
     pub fn dir_path(&self) -> &Path {
         &self.dir_path
+    }
+
+    pub fn package_hashes(&self) -> &BTreeMap<ProtobufPackageName, Sha1Hash> {
+        &self.package_hashes
     }
 
     pub fn proto_paths(&self) -> Vec<PathBuf> {
