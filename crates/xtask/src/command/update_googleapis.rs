5b3928d34f17907487eed239746343e7e01f6e96
bouzuya <m@bouzuya.net>
2024-08-23 10:41:08 +0900

    Fix update-googleapis subcommand

--- crates/xtask/src/command/update_googleapis.rs
+++ crates/xtask/src/command/update_googleapis.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 pub fn execute() -> anyhow::Result<()> {
     let googleapis_dir = PathBuf::from("crates/xtask/googleapis");
     let status = Command::new("git")
-        .args(["pull", "origin", "master"])
+        .args(["pull", "--rebase", "origin", "master"])
         .current_dir(&googleapis_dir)
         .status()?;
     anyhow::ensure!(status.success());
