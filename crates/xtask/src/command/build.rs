e9bdbb93e14328f11be0025185c9334842df1ff4
bouzuya <m@bouzuya.net>
2024-08-31 09:54:57 +0900

    Fix to use dirs::*

--- crates/xtask/src/command/build.rs
+++ crates/xtask/src/command/build.rs
@@ -2,7 +2,6 @@ mod build_crate;
 mod build_crates;
 
 use std::fs;
-use std::path::PathBuf;
 use std::str::FromStr as _;
 
 use crate::crate_name::CrateName;
@@ -42,10 +41,11 @@ use crate::state::State;
 /// ```
 pub fn execute(force_update: bool) -> anyhow::Result<()> {
     let generated_dir = dirs::generated_dir()?;
-    let xtask_dir = PathBuf::from("crates").join("xtask");
+    let googleapis_dir = dirs::googleapis_dir()?;
+    let xtask_dir = dirs::xtask_crate_dir()?;
 
     // load googleapis/
-    let proto_dir = xtask_dir.join("googleapis");
+    let proto_dir = googleapis_dir;
     let proto_dir = ProtoDir::load(proto_dir)?;
 
     // load state.json
--- crates/xtask/src/command/update_googleapis.rs
+++ crates/xtask/src/command/update_googleapis.rs
@@ -1,8 +1,9 @@
-use std::path::PathBuf;
 use std::process::Command;
 
+use crate::dirs;
+
 pub fn execute() -> anyhow::Result<()> {
-    let googleapis_dir = PathBuf::from("crates/xtask/googleapis");
+    let googleapis_dir = dirs::googleapis_dir()?;
     let status = Command::new("git")
         .args(["pull", "--rebase", "origin", "master:master"])
         .current_dir(&googleapis_dir)
