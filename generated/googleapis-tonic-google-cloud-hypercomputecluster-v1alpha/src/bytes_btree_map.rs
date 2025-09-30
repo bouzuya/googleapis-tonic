f875350e6bb9e562bbd545195396163408b4472a
bouzuya <m@bouzuya.net>
2025-09-30 15:10:38 +0000

    Regenerate crates using googleapis/google#e78280f4648373caffb79fe532866728625db2fe

---
+++ generated/googleapis-tonic-google-cloud-hypercomputecluster-v1alpha/src/bytes_btree_map.rs
@@ -0,0 +1,18 @@
+pub mod google {
+    pub mod api {
+        pub use googleapis_tonic_google_api::google::api::*;
+    }
+    pub mod cloud {
+        pub mod hypercomputecluster {
+            pub mod v1alpha {
+                include!("bytes_btree_map/google.cloud.hypercomputecluster.v1alpha.rs");
+            }
+        }
+    }
+    pub mod longrunning {
+        pub use googleapis_tonic_google_longrunning::google::longrunning::*;
+    }
+    pub mod rpc {
+        pub use googleapis_tonic_google_rpc::google::rpc::*;
+    }
+}
